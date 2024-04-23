8662ae2968fda2b67c6091568adf911cc0378222
jit66 <jeetthakur251@gmail.com>
2024-04-23 22:26:02 +0530

    package.json & server.js updated

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,15 @@ import userRoutes from "./routes/user.route.js";
 import exploreRoutes from "./routes/explore.route.js";
 import authRoutes from "./routes/auth.route.js";
 import connectMongoDB from "./db/connectMongoDB.js";
+import path from "path"
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve()
+
+console.log("dirname",__dirname)
 
 app.use(session({ secret: "keyboard cat", resave: false, saveUninitialized: false }));
 // Initialize Passport!  Also use passport.session() middleware, to support
@@ -22,16 +27,18 @@ app.use(passport.session());
 // Here we can remove the cors, it's not necessary in production because the frontend and backend are on the same domain. I forgot to mention that in the video, sorry about that.🙄
 app.use(cors());
 
-app.get("/", (req, res) => {
-	res.send("Server is ready");
-});
-
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*",(req,res) =>{
+	res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
+
 
 app.listen(5001, () => {
-	console.log(`Server started on http://localhost:5001`);
+	console.log(`Server started on http://localhost:${PORT}`);
 	connectMongoDB();
 });
