bb760484d240a7071fdd4f98b964dff6c08c2d87
jit66 <jeetthakur251@gmail.com>
2024-04-23 15:09:18 +0530

    server-api-routes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -17,19 +17,16 @@ const getUserProfileAndRepos = useCallback(
 	async(username="jit66") => {
 		setLoading(true);
 		try {
-		  const userRes = await fetch(`https://api.github.com/users/${username}`,{
-			headers: {
-				authorization : `token ${import.meta.env.VITE_GITHUB_API_TOKEN}`
-			}
-		  });
-		  const userProfile = await userRes.json();
-		  setUserProfile(userProfile);
-	  
-		  const repoRes = await fetch(userProfile.repos_url);
-		  const repos = await repoRes.json();
+		  
+		const res = await fetch(`http://localhost:5001/api/users/profile/${username}`)
+
+		const {repos ,userProfile} = await res.json()
+			console.log(userProfile ,"user profile")
+
 		  repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
 
 		  setRepos(repos);
+		  setUserProfile(userProfile)
 		  console.log("userProfile",userProfile)
 		  console.log("repos",repos)
 
